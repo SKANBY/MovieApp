8aa9e8a4153bd6f89a7a4484eb141514d7b8d530
김나영 <KNY@gimnayeongui-MacBook-Pro.local>
2018-02-03 15:36:12 +0900

    Loading states

--- src/App.js
+++ src/App.js
@@ -6,19 +6,16 @@ import Movie from './Movie';
 class App extends Component {
 
   state = {
-    movies: [
-      {
-        title: "신과 함께",
-        poster: "http://img.movist.com/?img=/x00/04/78/00_p1.jpg"
-      }
-    ]
   }
 
   componentDidMount(){
     setTimeout(() => {
       this.setState({
         movies: [
-          ...this.state.movies,
+          {
+            title: "신과 함께",
+            poster: "http://img.movist.com/?img=/x00/04/78/00_p1.jpg"
+          },
           {
             title: "염력",
             poster: "https://upload.wikimedia.org/wikipedia/ko/thumb/b/b2/%EC%98%81%ED%99%94_%EC%97%BC%EB%A0%A5.jpg/250px-%EC%98%81%ED%99%94_%EC%97%BC%EB%A0%A5.jpg"
@@ -27,12 +24,18 @@ class App extends Component {
       })
     }, 5000)
   }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    })
+    return movies
+  }
+
   render() {
     return (
       <div className="App">
-        {this.state.movies.map((movie, index) => {
-          return <Movie title={movie.title} poster={movie.poster} key={index} />
-        })}
+        {this.state.movies ? this._renderMovies() : 'Loading'}
       </div>
     );
   }
